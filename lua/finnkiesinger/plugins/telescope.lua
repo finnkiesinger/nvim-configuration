03cb622b79479734ead319b3db16f6dee7868b1f
Finn Kiesinger <finnkiesinger@gmail.com>
2024-11-24 15:02:10 +0100

    Configuration Changes

--- lua/finnkiesinger/plugins/telescope.lua
+++ lua/finnkiesinger/plugins/telescope.lua
@@ -19,9 +19,9 @@ return {
             ["<C-k>"] = actions.move_selection_previous,
             ["<C-j>"] = actions.move_selection_next,
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          }
-        }
-      }
+          },
+        },
+      },
     })
 
     telescope.load_extension("fzf")
@@ -30,8 +30,13 @@ return {
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwdFuzzy find recent files" })
+    keymap.set(
+      "n",
+      "<leader>fs",
+      "<cmd>Telescope live_grep<CR>",
+      { desc = "Find string in cwdFuzzy find recent files" }
+    )
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find string under cursor in cwd" })
-  end
+  end,
 }
